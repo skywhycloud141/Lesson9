b287faec89f1d6a103dff9fc9653e0274b31cb98
Sky <daniilanufriev0@gmail.com>
2026-01-19 23:08:42 +0500

    no bugs(maybe)

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const http = require("http");
 
-
 const PORT = 3000;
 
-const server = http.createServer(async(request,response) =>{
+const server = http.createServer(async (request, response) => {
     if (request.method === 'GET' && request.url === '/') {
-         response.setHeader('Content-Type', 'text/html; charset=utf-8');
+        response.setHeader('Content-Type', 'text/html; charset=utf-8');
         response.writeHead(200);
-       
 
         response.end(`<form action="/save" method="POST">
                 <input name="username" placeholder="Ваше имя" />
@@ -18,60 +16,57 @@ const server = http.createServer(async(request,response) =>{
 
         request.on('data', (chunk) => {
             body.push(chunk)
-
-            // console.log(`Прилетел кусочек данных:${chunk}`)
         });
-        request.on('end' , () => {
-            // chunk1 + chunk2 ... [chunk1(Buffer {4e , 5d}), chunk2]
-            const parsedBody = Buffer.concat(body).toString('utf-8');
 
+        request.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString('utf-8');
             const decodedString = decodeURIComponent(parsedBody);
 
             console.log(`Полное тело запроса:${decodedString}`);
 
-
-
-            response.setHeader('Content-Type' , 'text/plain; charset=utf-8')
-            response.statsuCode = 200;
+            response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+            response.statusCode = 200;
             response.end(`Данные получены! Вы прислали: ${decodedString}`)
         });
-    } else if (request.method === "GET" && 
+    } else if (request.method === "GET" &&
         (request.url === '/servers' || request.url === '/users')) {
-       
+
         const data = await fetch('https://gusic.xyz/stats')
-        const jsonData = await data.json(); // { servers: 92108, users: 4737414 }
+        const jsonData = await data.json();
 
-
-            response.setHeader('Content-Type' , 'text/plain; charset=utf-8')
-            response.statusCode = 200;
-
+        response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+        response.statusCode = 200;
 
         console.log(jsonData)
 
-        if (request.url === "/servers"){
-
-        response.end(`Количество серверов на данный момент: ${jsonData.servers}`);
+        if (request.url === "/servers") {
+            response.end(`Количество серверов на данный момент: ${jsonData.servers}`);
+        } else if (request.url === "/users") {
+            response.end(`Количество пользователей на данный момент: ${jsonData.users}`);
         }
-    }else if (request.url === "/users"){
-        response.end(`Количество пользователей на данный момент: ${jsonData.users}`); 
-        
-    
-    
-    } else if (request.method === "GET" && request.url === '/weather') {
-            const weather = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true")
-            const weatherData = await weather.json();
-            // console.log(weatherData)
+    } else if (request.method === "GET" &&
+        (request.url === '/weather' || request.url === '/wind')) {
+        const weather = await fetch("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true")
+        const weatherData = await newFunction(weather);
+        // console.log(weatherData)
 
-            response.setHeader('Content-Type' , 'text/plain; charset=utf-8')
-            response.statusCode = 200;
-            
+        response.setHeader('Content-Type', 'text/plain; charset=utf-8')
+        response.statusCode = 200;
 
-            if (request.url === "/wind"){
-                response.end(`Текущая температура: ${weatherData.windspeed}`);
-            }
-        } 
-        
-    });
+        if (request.url === "/wind") {
+            response.end(`Текущая Погода В Берлине Сопровождается Температурой:${weatherData.current_weather.temperature}°C и Скоростью Ветра:${weatherData.current_weather.windspeed} м/c`);
+        } else if (request.url === "/weather") {
+            response.end(`Текущая Погода В Берлине Сопровождается Температурой:${weatherData.current_weather.temperature}°C и Скоростью Ветра:${weatherData.current_weather.windspeed} м/c`);
+        }
+    } else {
+        response.statusCode = 404;
+        response.end("Страница не найдена");
+    }
+
+    async function newFunction(weather) {
+        return await weather.json();
+    }
+});
 
 server.listen(PORT);
-console.log(`Сервер был запущен по адресу: http://localhost:${PORT}`);
+console.log(`Сервер был запущен по адресу: http://localhost:${PORT}`)
